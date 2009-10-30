6d4d484c3e1ab527d4ad7907b38e1c4502db2514
Kevin Menard <nirvdrum@gmail.com>
2009-10-29 21:00:50 -0400

    Handle errors from multiple stylesheets.

--- spec/css_validator_parser_spec.rb
+++ spec/css_validator_parser_spec.rb
@@ -7,7 +7,6 @@ describe "CSS Validator Parser" do
   end
 
   describe "single stylesheet results" do
-
     before(:each) do
       @parser.parse(single_stylesheet_results)
       @uri = "http://localhost:3000/stylesheets/master.css?1256692632"
@@ -37,10 +36,53 @@ describe "CSS Validator Parser" do
     end
   end
 
+  describe "multiple stylesheet results" do
+    before(:each) do
+      @parser.parse(multiple_stylesheet_results)
+      @uris = ["http://staging.snap2twitter.com/stylesheets/master.css?1256667744", "http://staging.snap2twitter.com/stylesheets/facebox.css?1256667744"]
+    end
+
+    it "should return validation errors" do
+      @parser.errors.size.should == 2 # There should only be two URIs.
+      @parser.errors[@uris.first].size.should == 24
+      @parser.errors[@uris.last].size.should == 1
+
+      error = @parser.errors[@uris.first].first
+      error[:line].should == '36'
+      error[:type].should == 'parse-error'
+      error[:context].should == '#customer_left.customized, #customer_right.customized'
+      error[:subtype].should == 'exp'
+      error[:skipped_string].should == '5px'
+      error[:message].should == "Property -moz-border-radius doesn't exist :"
+
+      error = @parser.errors[@uris.last].first
+      error[:line].should == '60'
+      error[:type].should == 'parse-error'
+      error[:context].should == '* html #facebox_overlay'
+      error[:subtype].should == 'unrecognized'
+      error[:skipped_string].should == "document.body.scrollHeight > document.body.offsetHeight ? document.body.scrollHeight : document.body.offsetHeight + 'px')"
+      error[:message].should =~ /Value Error :  height \(http:\/\/www.w3.org\/TR\/CSS21\/visudet.html#propdef-height\)\s*Parse Error/
+    end
+
+    it "should return validation warnings" do
+      @parser.warnings.size.should == 1 # There should only be one URI.
+      @parser.warnings[@uri].size.should == 4 # That URI should have 4 warnings.
+
+      warning = @parser.warnings[@uri].first
+      warning[:line].should == '241'
+      warning[:level].should == '1'
+      warning[:message].should == 'Same colors for color and background-color in two contexts .user_info .comments .comment and .photo hr'
+    end
+  end
+
   private
 
   def single_stylesheet_results
     File.open(File.expand_path(File.dirname(__FILE__) + '/fixtures/single_stylesheet_with_errors.xml')).read
+  end
+
+  def multiple_stylesheet_results
+    File.open(File.expand_path(File.dirname(__FILE__) + '/fixtures/multiple_stylesheets_with_errors.xml')).read
   end
 
 end
